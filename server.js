274d3990c2eaad1e2caa3add5f30d096c66a3ba0
jonathonlaylo <jonathon.laylo@gmail.com>
2016-12-28 22:02:09 -1000

    made variables for headers, method, uri. Attempting to socket.write

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
+/*jshint esversion: 6 */
 const net = require(`net`);
-  
-  let server = net.createServer((socket) =>{
 
+  let server = net.createServer((socket) =>{
+    socket.setEncoding('utf8');
     socket.on('data', (chunk) => {
-      console.log(`vu${chunk}`);
+      // console.log(`${chunk}`);
+
+      let headers = chunk.split('\n');
+      console.log(`HEADERS: ${headers[0]}`);
+
+      let method = headers[0].split(' ')[0];
+      console.log(`METHOD: ${method}`);
+
+      let uri = headers[0].split(' ')[1];
+      console.log(`URI: ${uri}`);
+
+      socket.write('method:' + method);
+      socket.write('uri' + uri);
+      socket.end();
     });
 
   });
 
   server.listen(8080, "0.0.0.0", () =>{
-    console.log(`the current server is open`);
+    // console.log(`the current server is open`);
   });
 
 
