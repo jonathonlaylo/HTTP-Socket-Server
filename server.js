ac8810443d7489b3f4268dab58f504e9decb960b
raaarwwwwandy <hirandysan@gmail.com>
2016-12-30 10:43:15 -1000

    server isn't working, need to work on for loop to iterate between methods

--- server.js
+++ server.js
@@ -7,23 +7,40 @@ const content = require( "./content.js");
 
     socket.setEncoding('utf8');
     socket.on('data', (chunk) => {
-      // console.log(`${chunk}`);
+      console.log(`${chunk}`);
 
       let headers = chunk.split('\n');
-      console.log(`HEADERS: ${headers[0]}`);
+      // console.log(`HEADERS: ${headers[0]}`);
 
       let method = headers[0].split(' ')[0];
-      console.log(`METHOD: ${method}`);
+      // console.log(`METHOD: ${method}`);
 
       let uri = headers[0].split(' ')[1];
-      console.log(`URI: ${uri}`);
+      // console.log(`URI: ${uri}`);
 
-      socket.write('HTTP/1.1 200 OK');
-      socket.write('\nContent-Type: text/html; charset=utf-8');
-      socket.write(`\nContent-Length: ${content.index_html.length}`);
-      socket.write(`\n\n`);
-      socket.write(content.index_html);
-      socket.end();
+      let date = (new Date(Date.now()));
+      // console.log(`date: ${date}`);
+
+      // let response = `HTTP/1.1 200 OK 
+      //     \nContent-Type: text/html; charset=utf-8
+      //     \nContent-Length: ${content.index_html.length}
+      //     \nDate: ${date}
+      //     \nServer: Some Server
+      //     \n\n
+      //     content.index_html`;
+
+      //     console.log(response);
+
+      // function headerGenerater()
+            
+          // socket.write(`HTTP/1.1 200 OK`);
+          // socket.write(`\nContent-Type: text/html; charset=utf-8`);
+          // socket.write(`\nContent-Length: ${content.index_html.length}`);
+          // socket.write(`\nDate: ${date}`);
+          // socket.write(`\nServer: Some Server`);
+          // socket.write(`\n\n`);
+          // socket.write(content.index_html);
+          // socket.end();
     });
 
   });
